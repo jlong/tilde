7fafe306c7e2c30cd4642a5a17edbea53f72cce3
John W. Long <me@johnwlong.com>
2011-09-07 09:23:12 -0400

    make ~/Library visible; don't remember open windows in Safari

--- defaults.sh
+++ defaults.sh
@@ -6,14 +6,19 @@ if [ ! `defaults read com.apple.finder AppleShowAllFiles` == "0" ]; then
   killall Finder
 fi
 
-if [ ! `defaults read com.apple.Safari IncludeDebugMenu` = "1" ]; then
-  echo "Writing default: Show developer menu in Safari..."
-  defaults write com.apple.Safari IncludeDebugMenu 1
-  killall Safari
-fi
-
 if [ ! `defaults read com.apple.screencapture disable-shadow` = "1" ]; then
   echo "Writing default: Disable shadows on screenshots of windows..."
   defaults write com.apple.screencapture disable-shadow 1
   killall SystemUIServer
 fi
+
+echo "Writing default: Show developer menu in Safari..."
+defaults write com.apple.Safari IncludeDebugMenu 1
+
+echo "Writing default: Don't remember open windows in Safari..."
+defaults write com.apple.Safari NSQuitAlwaysKeepsWindows 0
+
+killall Safari
+
+# Make Library visible
+/Developer/Tools/SetFile -a v ~/Library/
